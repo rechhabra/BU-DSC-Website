885668b9e02f9f3452956d50cabe627695c2c056
Reshab Chhabra <33813644+rechhabra@users.noreply.github.com>
2020-10-09 17:22:03 -0400

    Add files via upload

---
+++ menubuttons.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+      $("li join a").hover(function(){
+        $(this).css("color", "#FABE08");
+        }, function(){
+        $(this).css("color", "#FFF");
+      });
+      $("li events a").hover(function(){
+        $(this).css("color", "#4284F5");
+        }, function(){
+        $(this).css("color", "#FFF");
+      });
+      $("li team a").hover(function(){
+        $(this).css("color", "#0C9D58");
+        }, function(){
+        $(this).css("color", "#FFF");
+      });
+      $("li about a").hover(function(){
+        $(this).css("color", "#EA4235");
+        }, function(){
+        $(this).css("color", "#FFF");
+      });
+    });
---
+++ script.js
@@ -0,0 +1,5 @@
+$("a").hover(function(){
+  $(this).css("background-color", "yellow");
+  }, function(){
+  $(this).css("background-color", "pink");
+});
